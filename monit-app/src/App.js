5a43c71772924f9cd73554c6dd173ce36d8dc891
taehee <taehi.dev@gmail.com>
2022-12-22 17:04:31 +0900

    First to main

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      첫화면
-    </div>
-  );
+  return <div className="App">메인화면</div>;
 }
 
 export default App;
