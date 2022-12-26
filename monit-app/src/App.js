2ba45111cedf6bb67879b903a703751d302bdb36
taehee <taehi.dev@gmail.com>
2022-12-26 20:39:51 +0900

    20221226

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="App">메인화면</div>;
-      <div>오늘은 크리스마스 이브이브 12/23</div>
+      <div>오늘은 크리스마스 다음날 12/26</div>
       <div>곧 새해입니다^^ </div>
     </>
   );
