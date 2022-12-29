382b2bfaf30a53b27159c4f48e66280b5ccee7bf
Noh <jin0201948@gmail.com>
2022-12-29 16:04:33 +0900

    Docs: History 내용 추가

---
+++ monit-app/src/Components/History.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <div className="History-container">
+      <h3>2022</h3>
+      <ol>
+        <li>11 건강보험공단 복지용구등록진행 </li>
+        <li>11 카톨릭대학 성빈센트병원 MOU체결</li>
+        <li>11 홍콩 Gerontechnology Platform Test Bed 품목 선정</li>
+        <li>10 홍콩병원협회 디지탈헬스케어 구매품목등록</li>
+        <li>10 뒤셀도르프 Diakonie 요양원 시범도입 </li>
+        <li>10 뒤셀도르프 레하케어 전시참가 </li>
+        <li>10 온누리요양원 MOU체결 시법도입 사업진행 </li>
+        <li>01 CES 2022 Samsung 관 전시</li>
+      </ol>
+      <h3>2021</h3>
+      <ol>
+        <li>10 카톨릭 대학병원 임상 테스트 진행</li>
+        <li>10 홍콩 Delta Pyramax 독점공급계약</li>
+        <li>09 코스트코 캐나다 입점 확정</li>
+        <li>
+          06 엘더리 케어 관련 고려대학교 안암/안산병원, 카톨릭 대학병원,
+          양평군요양원, 은혜의집 베타테스트 진행
+        </li>
+        <li>06 싱가포르 UnitedBMEC 독점공급계약 체결</li>
+      </ol>
+      <h3>2020</h3>
+      <ol>
+        <li>09 유한킴벌리 요양기관 대상 베타테스트 계약 체결</li>
+        <li>08 Series A 브릿지 펀드 유치</li>
+        <li>03 JPMA Innovation Award 수상</li>
+        <li>01 CES 2020 참가</li>
+      </ol>
+      <h3>2019</h3>
+      <ol>
+        <li>01 MONIT America Inc.설립(Delaware C-corp)</li>
+        <li>01 CES 2019 참가</li>
+        <li>01 SkyDeck Accelerator Cohort Team 선정(UC Berkeley Foundation)</li>
+      </ol>
+      <h3>2018</h3>
+      <ol>
+        <li>09 Series A 투자 유치</li>
+        <li>08 New York NOW참가</li>
+        <li>08 스마트 베이비 캐리어 'Bebefit' 출시</li>
+        <li>07 2018 KITAS 스마트 디바이스쇼 참가</li>
+        <li>06 스마트 베이비 모니터 '모닛' 출시</li>
+        <li>04 Orange Fab Asia Acceleration 선정</li>
+        <li>03 Kimberly Clark 공식 Supplier 선정</li>
+        <li>02 K-Global 300 선정</li>
+        <li>01 KIC Washington Bio Acceleration 선정</li>
+      </ol>
+      <h3>2017</h3>
+      <ol>
+        <li>10 유한킴벌리 스마트 프로비스 제공 MOU 계약 체결</li>
+        <li>09 Tech Crunch Disrupt SF 2017 참가</li>
+        <li>07 퍼스트 펭귄형 창업기업 선정</li>
+        <li>06 삼성벤처캐피탈 투자 유치</li>
+        <li>06 삼성전자 C-Lab Spinoff 및 법인 설립(서울 서초구)</li>
+      </ol>
+    </div>
+  );
+};
+
+export default History;
