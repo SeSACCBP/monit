516f40c9d692b404cb7c78ab9082a6a73c0c4498
Noh <jin0201948@gmail.com>
2022-12-30 12:03:46 +0900

    Docs: History component 여러개 방식

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -1,11 +1,30 @@
-import History from "./Components/History";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HistoryRoot from './Components/HistoryRoot';
+import History2020 from './Components/History2020';
+import History2021 from './Components/History2021';
+import History2022 from './Components/History2022';
+import History2019 from './Components/History2019';
 
-function App() {
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element:<HistoryRoot/>,
+    errorElement:<div>page not found</div>,
+    children:[
+      {index:true,element:<History2022/>},
+      {path:'/2021',element:<History2021/>},
+      {path:'/2020',element:<History2020/>},
+      {path:'/2019',element:<History2019/>}
+    ]
+  }
+])
+const App = () => {
   return (
-    <>
-      <History />
-    </>
+    <div>
+      <RouterProvider router={router}/>
+    </div>
   );
-}
+};
 
 export default App;
