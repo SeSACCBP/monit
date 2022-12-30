df42b85c9cd8e5a94a624b6117861b3a21f70631
Noh <jin0201948@gmail.com>
2022-12-30 15:38:28 +0900

    Feat: History commit

--- monit-app/src/Components/HistoryMenu.js
+++ monit-app/src/Components/HistoryMenu.js
@@ -8,8 +8,10 @@ const HistoryMenu = () => {
       <br />
       <Link to="/2021">2021</Link>
       <br />
-      <Link to="/2020">2020</Link><br />
-      <Link to="/2019">2019</Link><br />
+      <Link to="/2020">2020</Link>
+      <br />
+      <Link to="/2019">2019</Link>
+      <br />
     </div>
   );
 };
