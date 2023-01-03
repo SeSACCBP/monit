0f7d65990a08a8d5fcf4618582579fe3a784a980
DK <re4ess@gmail.com>
2023-01-03 09:39:18 +0900

    Feat : header & footer 추가

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -1,10 +1,15 @@
-import Introduction from "./Components/Introduction";
-import MainSectionComponent01 from "./Components/MainSectionComponent01";
-import Overview from "./Components/Overview";
+import Introduction from './Components/Introduction';
+import MainSectionComponent01 from './Components/MainSectionComponent01';
+import Overview from './Components/Overview';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
+      {/* 동교 작업용 */}
+      <Header />
+
       {/* 태희 작업용 */}
       {/* <Introduction /> */}
 
@@ -12,6 +17,9 @@ function App() {
       <Overview />
 
       {/* 수아 작업용 */}
+
+      {/* 동교 작업용 season 2 */}
+      <Footer />
     </>
   );
 }
