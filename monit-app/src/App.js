597b14af711ab33f85181cbd79d96cc445dc80b1
suconpa <thkWkddl100@gmail.com>
2023-01-03 10:15:10 +0900

    FEAT : import mainsection 3,4 추가

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -1,36 +1,36 @@
 import Introduction from "./Components/Introduction";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import MainSectionComponent01 from "./Components/MainSectionComponent01";
 import MainSectionComponent02 from "./Components/MainSectionComponent02";
-import MapComponent from "./Components/MapComponent";
+import MainSectionComponent03 from "./Components/MainSectionComponent03";
+import MainSectionComponent04 from "./Components/MainSectionComponent04";
+import Introduction from "./Components/Introduction";
 import Overview from "./Components/Overview";
+import MapComponent from "./Components/MapComponent";
 
 function App(props) {
   console.log(props);
-
   return (
     <>
       {/* 동교 작업용 */}
       <Header />
-
       {/* 태희 작업용 */}
       {/* <Introduction /> */}
       {/* 태희 작업용 */}
-      {/* <Introduction /> */}
-
+      <Introduction />
       {/* 율 작업용 */}
       {/* <Overview /> */}
-
       {/* 수아 작업용 */}
-
       {/* <MainSectionComponent01 /> */}
       {/* <MainSectionComponent02 /> */}
       {/* <MainSectionComponent03 /> */}
       {/* <MainSectionComponent04 /> */}
-
       {/* 나은 */}
       <MapComponent />
+      {/* 동교 작업용 season 2 */}
+      <Footer />
     </>
   );
 }
-
 export default App;
