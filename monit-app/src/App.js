6fb55f5dc8a2ec12fbe4ff7606bf1ba9b42f521b
suconpa <thkWkddl100@gmail.com>
2023-01-04 12:24:00 +0900

    Style : main.scss 수정

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -17,16 +17,16 @@ function App(props) {
       {/* <Header /> */}
 
       {/* 태희 작업용 */}
-      <Introduction />
+      {/* <Introduction /> */}
 
       {/* 율 작업용 */}
       {/* <Overview /> */}
 
       {/* 수아 작업용 */}
       {/* <MainSectionComponent01 /> */}
-      {/* <MainSectionComponent02 /> */}
-      {/* <MainSectionComponent03 /> */}
-      {/* <MainSectionComponent04 /> */}
+      <MainSectionComponent02 />
+      <MainSectionComponent03 />
+      <MainSectionComponent04 />
 
       {/* 나은 */}
       {/* <MapComponent /> */}
