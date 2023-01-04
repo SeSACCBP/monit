08e780c40317d1e7744110e8b63d7e3674330959
yuuulya <yla101@naver.com>
2023-01-04 17:11:14 +0900

    FEAT : overview페이지 css단위변경(반응형)

--- monit-app/src/Components/Overview.jsx
+++ monit-app/src/Components/Overview.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import Vision from "./Vision";
 import Spinoff from "./Spinoff";
 import "../scss/Overview.scss";
+import HeaderOver from "./SubHeader/HeaderOver";
 
 const Overview = () => {
   return (
-    <div className="overview-section">
-      <div className="overivew-bg"></div>
-      {/* vision */}
-      <Vision />
-      <div className="overview-middlebar"></div>
-      {/* spinoff story */}
-      <Spinoff />
+    <div>
+      <HeaderOver />
+      <div className="overview-section">
+        <div className="overivew-bg"></div>
+        {/* vision */}
+        <Vision />
+        <div className="overview-middlebar"></div>
+        {/* spinoff story */}
+        <Spinoff />
+      </div>
     </div>
   );
 };
