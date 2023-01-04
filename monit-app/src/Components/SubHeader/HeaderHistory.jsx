4564e8bca00f87aa082cbf5e57fbd4ed82f289c1
naeun <gongwillson3@gmail.com>
2023-01-04 11:48:28 +0900

    Feat : 서브헤더 초안

---
+++ monit-app/src/Components/SubHeader/HeaderHistory.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "../scss/style.scss";
+import "../scss/SubHeader.scss";
+
+const HeaderHistory = () => {
+  return <div></div>;
+};
+
+export default HeaderHistory;
---
+++ monit-app/src/Components/SubHeader/HeaderLo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../scss/style.scss";
+import "../../scss/SubHeader.scss";
+
+const HeaderLo = () => {
+  return (
+    <div className="HeaderLo">
+      <div className="HeaderLo-Img">오시는 길</div>
+      <div className="HeaderLo-word">회사 소개 &gt; 오시는 길</div>
+    </div>
+  );
+};
+
+export default HeaderLo;
