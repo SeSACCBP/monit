d0de88545a8f031a5a0842d403cc698b0903a027
DK <re4ess@gmail.com>
2023-01-05 10:13:39 +0900

    Feat : header 메뉴

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -1,19 +1,19 @@
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import MainSectionComponent01 from "./Components/MainSectionComponent01";
-import MainSectionComponent02 from "./Components/MainSectionComponent02";
-import MainSectionComponent03 from "./Components/MainSectionComponent03";
-import MainSectionComponent04 from "./Components/MainSectionComponent04";
-import Introduction from "./Components/Introduction";
-import Overview from "./Components/Overview";
-import MapComponent from "./Components/MapComponent";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import MainSectionComponent01 from './Components/MainSectionComponent01';
+import MainSectionComponent02 from './Components/MainSectionComponent02';
+import MainSectionComponent03 from './Components/MainSectionComponent03';
+import MainSectionComponent04 from './Components/MainSectionComponent04';
+import Introduction from './Components/Introduction';
+import Overview from './Components/Overview';
+import MapComponent from './Components/MapComponent';
 
 function App(props) {
   console.log(props);
   return (
     <>
       {/* 동교 작업용 */}
-      {/* <Header /> */}
+      <Header />
 
       {/* 태희 작업용 */}
       {/* <Introduction /> */}
@@ -23,17 +23,17 @@ function App(props) {
 
       {/* 수아 작업용 */}
       <MainSectionComponent01 />
-      <MainSectionComponent02 />
-      <MainSectionComponent03 />
-      <MainSectionComponent04 />
+      {/* <MainSectionComponent02 /> */}
+      {/* <MainSectionComponent03 /> */}
+      {/* <MainSectionComponent04 /> */}
 
       {/* 나은 */}
       {/* <MapComponent /> */}
-      
+
       {/*은기 */}
 
       {/* 동교 작업용 season 2 */}
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
