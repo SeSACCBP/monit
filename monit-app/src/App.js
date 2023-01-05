2131b0312689ec8d85e2231dd4e9a50efe8fd932
Noh <jin0201948@gmail.com>
2023-01-05 11:13:55 +0900

    feat: history scss 수정

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -1,43 +1,35 @@
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import MainSectionComponent01 from "./Components/MainSectionComponent01";
-import MainSectionComponent02 from "./Components/MainSectionComponent02";
-import MainSectionComponent03 from "./Components/MainSectionComponent03";
-import MainSectionComponent04 from "./Components/MainSectionComponent04";
-import Introduction from "./Components/Introduction";
-import Overview from "./Components/Overview";
-import MapComponent from "./Components/MapComponent";
-import History from "./Components/History";
-import Contact from "./Components/Contact";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import MainSectionComponent01 from './Components/MainSectionComponent01';
+import MainSectionComponent02 from './Components/MainSectionComponent02';
+import MainSectionComponent03 from './Components/MainSectionComponent03';
+import MainSectionComponent04 from './Components/MainSectionComponent04';
+import Introduction from './Components/Introduction';
+import Overview from './Components/Overview';
+import MapComponent from './Components/MapComponent';
 
 function App(props) {
   console.log(props);
   return (
     <>
       {/* 동교 작업용 */}
-      {/* <Header /> */}
+      <Header />
 
       {/* 태희 작업용 */}
       {/* <Introduction /> */}
-
-      {/* 율 작업용 */}
-      <Overview />
-
       {/* 수아 작업용 */}
-      {/* <MainSectionComponent01 /> */}
-      {/* <MainSectionComponent02 />
+      <MainSectionComponent01 />
+      <MainSectionComponent02 />
       <MainSectionComponent03 />
-      <MainSectionComponent04 /> */}
+      <MainSectionComponent04 />
 
       {/* 나은 */}
       {/* <MapComponent /> */}
-      {/* <Contact /> */}
 
       {/*은기 */}
-      {/* <History /> */}
 
       {/* 동교 작업용 season 2 */}
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
