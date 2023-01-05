84d68babc06eae2505369c407ddd9e41d66df6c8
taehee <taehi.dev@gmail.com>
2023-01-05 10:31:23 +0900

    Build : Introduction-서브헤더추가

--- monit-app/src/Components/Introduction.jsx
+++ monit-app/src/Components/Introduction.jsx
@@ -2,11 +2,10 @@ import "../scss/Introduction.scss";
 import logo from "../imgs/Fragment_img/monitLogo_white.png";
 import HeaderIntro from "./SubHeader/HeaderIntro";
 
-
-
 function Introduction() {
   return (
     <>
+      <HeaderIntro />
       <div>
         <div className="introduction-items-wrapper">
           {/* ---------introduction-title--------- */}
