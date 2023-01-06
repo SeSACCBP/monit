bd98c1368b616934e013036d031ca146ff92faa5
Noh <jin0201948@gmail.com>
2023-01-06 10:17:36 +0900

    feat: history 서브헤더붙임

--- monit-app/src/Components/History.jsx
+++ monit-app/src/Components/History.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../scss/History.scss";
+import HeaderHistory from "./SubHeader/HeaderHistory";
 
 const History = () => {
   const years = [
@@ -130,74 +131,76 @@ const History = () => {
   }
 
   return (
-    <div className="history">
-      <div className="history-title">회사 연혁</div>
-      <div className="history-mainBox">
-        <div className="history-leftBox">
-          <div className="history-line"></div>
+    <>
+      <HeaderHistory />
+      <div className="history">
+        <div className="history-title">회사 연혁</div>
+        <div className="history-mainBox">
+          <div className="history-leftBox">
+            <div className="history-line"></div>
 
-          <input
-            className="history-but1"
-            type="radio"
-            value="2022"
-            id="history-inputId"
-            name="tory"
-            onClick={buttonOnclick}
-          ></input>
-          <br />
-          <input
-            className="history-but2"
-            type="radio"
-            value="2021"
-            id="history-inputId"
-            name="tory"
-            onClick={buttonOnclick}
-          ></input>
-          <br />
-          <input
-            className="history-but3"
-            type="radio"
-            value="2020"
-            id="history-inputId"
-            name="tory"
-            onClick={buttonOnclick}
-          ></input>
-          <br />
-          <input
-            className="history-but4"
-            type="radio"
-            value="2019"
-            id="history-inputId"
-            name="tory"
-            onClick={buttonOnclick}
-          ></input>
-          <br />
-          <input
-            className="history-but5"
-            type="radio"
-            value="2018"
-            id="history-inputId"
-            name="tory"
-            onClick={buttonOnclick}
-          ></input>
-          <br />
-          <input
-            className="history-but6"
-            type="radio"
-            value="2017"
-            id="history-inputId"
-            name="tory"
-            onClick={buttonOnclick}
-          ></input>
-
+            <input
+              className="history-but1"
+              type="radio"
+              value="2022"
+              id="history-inputId"
+              name="tory"
+              onClick={buttonOnclick}
+            ></input>
+            <br />
+            <input
+              className="history-but2"
+              type="radio"
+              value="2021"
+              id="history-inputId"
+              name="tory"
+              onClick={buttonOnclick}
+            ></input>
+            <br />
+            <input
+              className="history-but3"
+              type="radio"
+              value="2020"
+              id="history-inputId"
+              name="tory"
+              onClick={buttonOnclick}
+            ></input>
+            <br />
+            <input
+              className="history-but4"
+              type="radio"
+              value="2019"
+              id="history-inputId"
+              name="tory"
+              onClick={buttonOnclick}
+            ></input>
+            <br />
+            <input
+              className="history-but5"
+              type="radio"
+              value="2018"
+              id="history-inputId"
+              name="tory"
+              onClick={buttonOnclick}
+            ></input>
+            <br />
+            <input
+              className="history-but6"
+              type="radio"
+              value="2017"
+              id="history-inputId"
+              name="tory"
+              onClick={buttonOnclick}
+            ></input>
+          </div>
+          <ul className="history-textBox">
+            {arr.map((one) => {
+              return <li>{one}</li>;
+            })}
+          </ul>
         </div>
-        <ul className="history-textBox">
-          {arr.map((one) => {
-            return <li>{one}</li>;
-          })}
-        </ul>
       </div>
-    </div>
+    </>
   );
 };
 
