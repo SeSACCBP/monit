220a405871262e672585d9366737f4f0656cdb33
yuuulya <95074363+yuuulya@users.noreply.github.com>
2023-01-06 14:59:33 +0900

    FIX : App.js return 문 수정

--- monit-app/src/App.js
+++ monit-app/src/App.js
@@ -1,25 +1,24 @@
-import Introduction from "./Components/Introduction";
-import Overview from "./Components/Overview";
-import MapComponent from "./Components/MapComponent";
-import History from "./Components/History";
-import Contact from "./Components/Contact";
-import Main from "./Components/Main.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./Components/Root";
-import Error404 from "./Components/Error404";
+import Introduction from './Components/Introduction';
+import Overview from './Components/Overview';
+import MapComponent from './Components/MapComponent';
+import History from './Components/History';
+import Contact from './Components/Contact';
+import Main from './Components/Main.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './Components/Root';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     errorElement: <Overview />,
     children: [
       { index: true, element: <Main /> },
-      { path: "/overview", element: <Overview /> },
-      { path: "/introduction", element: <Introduction /> },
-      { path: "/history", element: <History /> },
-      { path: "/location", element: <MapComponent /> },
-      { path: "/contact", element: <Contact /> },
+      { path: '/overview', element: <Overview /> },
+      { path: '/introduction', element: <Introduction /> },
+      { path: '/history', element: <History /> },
+      { path: '/location', element: <MapComponent /> },
+      { path: '/contact', element: <Contact /> },
     ],
   },
 ]);
@@ -28,8 +27,7 @@ function App(props) {
   console.log(props);
   return (
     <>
-      {/* <RouterProvider router={router} /> */}
-      <Error404 />
+      <RouterProvider router={router} />
     </>
   );
 }
