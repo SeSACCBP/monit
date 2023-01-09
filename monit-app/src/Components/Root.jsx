80b4b00fac683b0f5c13f1cff17ba843f77d66f7
yuuulya <95074363+yuuulya@users.noreply.github.com>
2023-01-09 10:30:37 +0900

    Update Root.jsx

--- monit-app/src/Components/Root.jsx
+++ monit-app/src/Components/Root.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
-import Header from './Header';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Root = () => {
   return (
