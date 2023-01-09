d74f6107f5b88b022444ed664d5835a014cb7762
DK <re4ess@gmail.com>
2023-01-09 16:46:08 +0900

    FEAT:헤더 수정

--- monit-app/src/Components/Header.jsx
+++ monit-app/src/Components/Header.jsx
@@ -4,28 +4,29 @@ import { Link } from 'react-router-dom';
 import BackTopButton from './BackTopButton';
 
 const Header = () => {
-  // const header_area = document.querySelector('.header-list');
-  // const back_area = document.querySelector('.header-back');
+  const [headerOn, setHeaderOn] = useState(true);
 
-  // header_area.addEventListener('mouseover', function () {
-  //   back_area.classList.add('active');
-  // });
-  // header_area.addEventListener('mouseout', function () {
-  //   back_area.classList.remove('active');
-  // });
+  const [ham, setHam] = useState(true);
 
-  const [isOn, setisOn] = useState(false);
+  const [isOn1, setisOn1] = useState(true);
 
   const buttonClick = (e) => {
-    // console.log(e.target);
-    setisOn((current) => !current);
+    setHam((current) => !current);
   };
+
+  const [isOn2, setisOn2] = useState(true);
+
+  const [isOn3, setisOn3] = useState(true);
 
   return (
     <div className="header-line">
       <nav className="header-area">
         <a className="header-imgArea" href="/"></a>
-        <ul className="header-list">
+        <ul
+          className="header-list"
+          onMouseOver={() => setHeaderOn(false)}
+          onMouseOut={() => setHeaderOn(true)}
+        >
           <li className="header-title">
             <a href="#">회사 소개</a>
             <ul className="header-sub-list">
@@ -67,13 +68,15 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-      <div className="header-back"></div>
+
+      <div className={headerOn ? 'header-back' : 'header-back-active'}></div>
+
       <nav className="header-area-m">
         <div className="header-imgArea-m">
           <a className="header-img-m" href="/"></a>
         </div>
         <div className="hamburger-area">
-          <button className="hamburger_btn" onClick={buttonClick} href="#">
+          <button className="hamburger-btn" onClick={buttonClick} href="#">
             <div className="hamburger">
               <span className="bar"></span>
               <span className="bar"></span>
@@ -81,21 +84,32 @@ const Header = () => {
             </div>
           </button>
         </div>
-        {/* <div className="header-list-m"> */}
-        <div className={isOn ? 'header-list-m' : 'header-list-m-on'}>
+        <div className={ham ? 'header-list-m' : 'header-list-m-on'}>
           <ul className="header-list-m-depth1">
             <li className="header-title-m" id="company_intro">
-              <a href="#company_intro">회사 소개</a>
-              <div className="header-sub-list-m">
+              <a href="#company_intro" onClick={() => setisOn1(!isOn1)}>
+                회사 소개
+              </a>
+              <div
+                className={
+                  isOn1 ? 'header-sub-list-m-hide' : 'header-sub-list-m'
+                }
+              >
                 <a href="#">기업 이념</a>
-                <a href="#">사업 개오</a>
+                <a href="#">사업 개요</a>
                 <a href="#">회사 연혁</a>
                 <a href="#">오시는 길</a>
               </div>
             </li>
             <li className="header-title-m" id="product_intro">
-              <a href="#product_intro">재품 소개</a>
-              <div className="header-sub-list-m">
+              <a href="#product_intro" onClick={() => setisOn2(!isOn2)}>
+                제품 소개
+              </a>
+              <div
+                className={
+                  isOn2 ? 'header-sub-list-m-hide' : 'header-sub-list-m'
+                }
+              >
                 <a href="#">맥스 자세히 보기</a>
               </div>
             </li>
@@ -103,8 +117,14 @@ const Header = () => {
               <a href="#">구매 하기</a>
             </li>
             <li className="header-title-m" id="contact_us">
-              <a href="#contact_us">문의 하기</a>
-              <div className="header-sub-list-m">
+              <a href="#contact_us" onClick={() => setisOn3(!isOn3)}>
+                문의 하기
+              </a>
+              <div
+                className={
+                  isOn3 ? 'header-sub-list-m-hide' : 'header-sub-list-m'
+                }
+              >
                 <a href="#">제품 문의</a>
                 <a href="#">제휴 문의</a>
               </div>
