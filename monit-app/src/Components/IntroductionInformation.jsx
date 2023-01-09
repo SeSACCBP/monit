728faa111975ab8e1bfb894406e65a43d797fef7
taehee <taehi.dev@gmail.com>
2023-01-09 17:35:32 +0900

    Chore : inrroduction Mindmap - line 색 변경

--- monit-app/src/Components/IntroductionInformation.jsx
+++ monit-app/src/Components/IntroductionInformation.jsx
@@ -20,24 +20,24 @@ const IntroductionInformation = () => {
 
       {/* ---------introduction-index--------- */}
 
-      <div className="introductin-index-wrapper">
-        <span className="introductin-index-fontcolor">기업명</span>
+      <div className="introduction-index-wrapper">
+        <span className="introduction-index-fontcolor">기업명</span>
         &ensp;주식회사 모닛
         <br />
-        <span className="introductin-index-fontcolor">대표자</span>
+        <span className="introduction-index-fontcolor">대표자</span>
         &ensp;박도형
         <br />
-        <span className="introductin-index-fontcolor">설립일</span>
+        <span className="introduction-index-fontcolor">설립일</span>
         &ensp;2017.04.10
         <br />
-        <span className="introductin-index-fontcolor">소재지</span>
+        <span className="introduction-index-fontcolor">소재지</span>
         &ensp;서울시 서초구 효령로 40 기탄빌딩 3층
         <br />
-        <span className="introductin-index-fontcolor">업종</span>
+        <span className="introduction-index-fontcolor">업종</span>
         &emsp;&ensp;연구개발업 | 통신판매업 | 포털 및 기타 인터넷 정보 | 매개
         서비스업 | 소프트웨어 개발업 | 미디어 콘텐츠 창작업
         <br />
-        <span className="introductin-index-fontcolor">주요제품</span>
+        <span className="introduction-index-fontcolor">주요제품</span>
         &emsp;MECS 욕창방지 기저귀 케어 시스템 | MONIT 스마트 베이비 모니터 |
         MONIT Digital Therapeutics | Bebefit 스마트 베이비 케어 제품군
         <br />
