a136c28e692465c5527c1a3ddc6a75c6efa87bc4
yuuulya <yla101@naver.com>
2023-01-15 19:46:45 +0900

    FEAT : 문의하기 페이지 1차 완성, header에 문의하기 페이지 link연결

---
+++ monit-app/src/Components/Product.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SubHeader from "./SubHeader";
+import "../scss/Contact.scss";
+import { FaRegUserCircle, FaRegEnvelope } from "react-icons/fa";
+import Contact from "./Contact";
+
+const Product = () => {
+  const title = {
+    pageSection: "문의 하기",
+    pageTitle: "제픔 문의",
+    img: "SubHeader-Img-Inq",
+  };
+  const text = `제품에 관해 문의하실 내용을 보내주시면
+빠르게 확인 후 회신드리겠습니다.`;
+  return (
+    <>
+      <SubHeader title={title} />
+      <div className="map-title">
+        Contact Us
+        <p>제품 문의</p>
+      </div>
+      <Contact text={text} />
+    </>
+  );
+};
+
+export default Product;
