2838c0b590fe780334975e189788bde461a81ad4
yuuulya <yla101@naver.com>
2023-01-15 17:15:03 +0900

    FEAT : overview 모바일 반응형

--- monit-app/src/Components/Spinoff.jsx
+++ monit-app/src/Components/Spinoff.jsx
@@ -2,23 +2,21 @@ import React from "react";
 
 const Spinoff = () => {
   return (
-    <div className="overview" data-aos="fadeInUp">
+    <div className="overview-main-section" data-aos="fadeInUp">
       <div className="overview-title">
         <div className="overview-title-circle"></div>
         <div className="overview-title-text">Spin-Off Story</div>
         <div className="overview-title-bg"></div>
       </div>
-      <div className="overview-bar">
+      <div className="overview-bar-section">
         <div className="overview-bar"></div>
         <div className="overview-bar-circle"></div>
       </div>
       <div className="overview-content">
-        <span className="overview-content-b">모닛</span>은 2016년 삼성전자
-        C-Lab의 창의 개발 프로젝트로 시작되었습니다. <br />
-        사업 모델 검증을 성공적으로 마친 후, <br />
-        2017년 삼성벤처투자의 펀딩으로독자 법인을 설립하였습니다. <br />
-        엔지니어 기반의 공동창업자들이 인하우스 역량으로 모든 개발을 진행,{" "}
-        <br />
+        <span className="overview-content-bold">모닛</span>은 2016년 삼성전자
+        C-Lab의 창의 개발 프로젝트로 시작되었습니다. 사업 모델 검증을 성공적으로
+        마친 후, <br />
+        2017년 삼성벤처투자의 펀딩으로 독자 법인을 설립하였습니다. 엔지니어 기반의 공동창업자들이 인하우스 역량으로 모든 개발을 진행하며,
         인공지능 알고리즘을 통한 케어 시스템으로 전세계적으로 인정받고 있습니다.
       </div>
     </div>
