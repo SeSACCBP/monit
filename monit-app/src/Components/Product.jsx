3bd31cc1309b740aee626d502d9ce8f5095a7923
yuuulya <yla101@naver.com>
2023-01-23 21:19:06 +0900

    Chore : overview, contact 수정
    - overview :  하단 여백 수정
    - contact : 전체적인 문의창 크기 축소, 페이지 설명문 문단바꾸기, 전체적인 radius 수정

--- monit-app/src/Components/Product.jsx
+++ monit-app/src/Components/Product.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import SubHeader from './SubHeader';
-import '../scss/Contact.scss';
-import { FaRegUserCircle, FaRegEnvelope } from 'react-icons/fa';
-import Contact from './Contact';
+import React from "react";
+import SubHeader from "./SubHeader";
+import Contact from "./Contact";
+import { FaRegUserCircle, FaRegEnvelope } from "react-icons/fa";
 
 const Product = () => {
   const title = {
-    pageSection: '문의 하기',
-    pageTitle: '제품 문의',
-    img: 'SubHeader-Img-Inq',
+    pageSection: "문의 하기",
+    pageTitle: "제품 문의",
+    img: "SubHeader-Img-Inq",
   };
-  const text = `제품에 관해 문의하실 내용을 보내주시면
-빠르게 확인 후 회신드리겠습니다.`;
+  const text = [
+    "제품에 관해 문의하실 내용을 보내주시면",
+    <br />,
+    "빠르게 확인 후 회신드리겠습니다.",
+  ];
   return (
     <>
       <SubHeader title={title} />
